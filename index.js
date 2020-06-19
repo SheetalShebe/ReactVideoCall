a1df56d04399b365d54033e8b11281bf841357f6
sheetalshebe <sheetal@zool.in>
2020-06-19 16:18:04 +0530

    First commit for video call app

---
+++ index.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from 'react-native';
+import RouterComponent from './components/Router';
+import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => RouterComponent)
